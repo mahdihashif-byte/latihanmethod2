5666e42ff4111b87e9fd5aafd4e2e35ef815fee0
mahdihashif-byte <mahdihashif@gmail.com>
2026-01-21 15:19:11 +0700

    yey

--- latihanmethod2/Program.cs
+++ latihanmethod2/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int jumlahitem, hargaItem;
+decimal diskon, totalbayar;
+
+decimal hitungdiskon(int jml_item)
+{
+    if (jml_item == 5)
+        return 50000;
+    else if (jml_item == 10)
+        return 75000;
+    else
+        return 0;
+}
+
+decimal hitungTotalBayar(int jml_item, int harga_item, decimal diskon)
+{
+    return jml_item * harga_item - diskon;
+}
+
+Console.WriteLine("-_- Selamat datang di kasir -_- ");
+Console.Write("Masukkan jumlah item di beli:");
+jumlahitem = int.Parse(Console.ReadLine());
+
+Console.Write("Masukkan harga item: ");
+hargaItem = int.Parse(Console.ReadLine());
+
+diskon = hitungdiskon(jumlahitem);
+
+totalbayar = hitungTotalBayar(jumlahitem, hargaItem, diskon);
+
+Console.WriteLine($"Total bayar: Rp {totalbayar}");
